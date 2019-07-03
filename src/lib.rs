0f3d8bedfcfbeb272206b51ccc30e3db6de27171
andylokandy <andylokandy@hotmail.com>
2019-07-04 01:58:08 +0800

    Improve performace on sparse keys

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,130 @@
+use rand::{thread_rng, Rng};
 use std::collections::HashMap;
-use std::thread;
+use std::iter;
+use std::sync::mpsc::channel;
+use threadpool::ThreadPool;
+
+enum Message {
+    SmallGroupResult(HashMap<i64, (i64, i64)>),
+    LargeGroupResult(HashMap<i64, (i64, i64)>),
+}
 
 pub fn group_by_avg(pairs: &[(i64, i64)]) -> Vec<(i64, f64)> {
+    if pairs.len() == 0 {
+        return Vec::new();
+    }
+
     let num = num_cpus::get().min(pairs.len());
 
-    let handles: Vec<_> = (0..num)
-        .map(|i| {
-            // split chunks excuted in parallel
-            let len = pairs.len();
-            let chunk_size = len / num;
-            let chunk = if i < num - 1 {
-                Vec::from(&pairs[i * chunk_size..(i + 1) * chunk_size])
-            } else {
-                Vec::from(&pairs[i * chunk_size..])
-            };
+    let large_groups = detect_large_groups(pairs);
 
-            thread::spawn(move || {
-                let mut map: HashMap<i64, (i64, i64)> = HashMap::new();
+    let small_group_plan_threads = num;
+    let large_group_plan_threads = num;
 
-                // group value into the form (k) -> (count, sum) for each chuncks
-                for (k, v) in chunk {
-                    let &mut (ref mut count, ref mut sum) = map.entry(k).or_default();
-                    *count += 1;
-                    *sum += v;
-                }
-
-                map
-            })
-        })
+    let mut small_group_plan: Vec<Vec<(i64, i64)>> = iter::repeat(Vec::new())
+        .take(small_group_plan_threads)
+        .collect();
+    let mut large_group_plan: Vec<Vec<(i64, i64)>> = iter::repeat(Vec::new())
+        .take(large_group_plan_threads)
         .collect();
 
-    let mut collecter: HashMap<i64, (i64, i64)> = HashMap::new();
-
-    for handle in handles {
-        let map = handle.join().expect("something went wrong");
-
-        // merge results from all worker threads
-        for (k, v) in map {
-            let &mut (ref mut count, ref mut sum) = collecter.entry(k).or_default();
-            *count += v.0;
-            *sum += v.1;
+    let mut large_pair_count = 0;
+    for pair in pairs {
+        if large_groups.contains(&pair.0) {
+            large_group_plan[large_pair_count % large_group_plan_threads].push(*pair);
+            large_pair_count += 1;
+        } else {
+            small_group_plan[pair.0 as usize % small_group_plan_threads].push(*pair);
         }
     }
 
+    let (tx, rx) = channel::<Message>();
+    let pool = ThreadPool::new(num);
+
+    for plan in small_group_plan {
+        let tx = tx.clone();
+        pool.execute(move || {
+            tx.send(Message::SmallGroupResult(group_count_sum(&plan)))
+                .unwrap();
+        });
+    }
+
+    for plan in large_group_plan {
+        let tx = tx.clone();
+        pool.execute(move || {
+            tx.send(Message::LargeGroupResult(group_count_sum(&plan)))
+                .unwrap();
+        });
+    }
+
+    let mut collector: Vec<(i64, (i64, i64))> = Vec::new();
+    let mut large_group_collector: HashMap<i64, (i64, i64)> = HashMap::new();
+
+    for _ in 0..(small_group_plan_threads + large_group_plan_threads) {
+        let msg = rx.recv().unwrap();
+        match msg {
+            Message::SmallGroupResult(map) => {
+                collector.extend(map.into_iter());
+            }
+            Message::LargeGroupResult(map) => {
+                // merge results from all worker threads
+                for (k, v) in map {
+                    let &mut (ref mut count, ref mut sum) = large_group_collector.entry(k).or_insert((0, 0));
+                    *count += v.0;
+                    *sum += v.1;
+                }
+            }
+        }
+    }
+
+    collector.extend(large_group_collector.into_iter());
+
     // calc the avg
-    collecter
+    collector
         .into_iter()
         .map(|(k, (count, sum))| (k, sum as f64 / count as f64))
         .collect()
+}
+
+fn group_count_sum(pairs: &[(i64, i64)]) -> HashMap<i64, (i64, i64)> {
+    let mut map = HashMap::new();
+
+    for (k, v) in pairs {
+        let &mut (ref mut count, ref mut sum) = map.entry(*k).or_insert((0, 0));
+        *count += 1;
+        *sum += v;
+    }
+
+    map
+}
+
+const SAMPLES_COUNT: usize = 100;
+const SAMPLES_THRESHOLD: usize = 5;
+
+fn detect_large_groups(pairs: &[(i64, i64)]) -> Vec<i64> {
+    let len = pairs.len();
+
+    // if len < num_cpus::get() * 100 {
+    //     return Vec::new();
+    // }
+
+    let mut rng = thread_rng();
+    let mut samples: HashMap<i64, usize> = HashMap::new();
+
+    for _ in 0..SAMPLES_COUNT {
+        let sample = pairs[rng.gen_range(0, len)];
+        *samples.entry(sample.0).or_insert(0) += 1;
+    }
+
+    let mut large_groups = Vec::new();
+
+    for (k, v) in samples {
+        if v >= SAMPLES_THRESHOLD {
+            large_groups.push(k);
+        }
+    }
+
+    large_groups
 }
 
 #[test]
